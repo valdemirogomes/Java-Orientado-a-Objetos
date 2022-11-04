5aea3a28683542d2d140e805220d81a1f2224c4f
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-11-04 00:02:27 -0300

    Trabalhando com classes em OO

---
+++ src/classe/Pessoa.java
@@ -0,0 +1,52 @@
+package classe;
+
+import java.util.Date;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private int rg;
+	private String cpf;
+	private String dtNascimento;
+	
+	
+	public Pessoa(String nome, String sobrenome, int rg, String cpf, String dtNascimento) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.dtNascimento = dtNascimento;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public int getRg() {
+		return rg;
+	}
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+	
+}
